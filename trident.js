e60b7e539f3617837d5ee92e0f365ab56b17975c
Robert Wolf <robert.wolf@adelaide.edu.au>
2018-08-31 16:53:19 +0930

    Work-in-progress....

--- trident.js
+++ trident.js
@@ -8,13 +8,34 @@ var removeTridents = function () {var ind = 0; while (ind < tridents . length) {
 var showTridents = function () {for (var ind in tridents) console . log (tridents [ind]); console . log ('====');};
 var simulateTridents = function (delta) {for (var ind in tridents) tridents [ind] . simulate (delta);};
 var drawTridents = function (ctx) {for (var ind in tridents) tridents [ind] . draw (ctx);};
+var classifyTridents = function (trident) {for (var ind in tridents) tridents [ind] . status = tridents [ind] . checkStatusOf (trident);};
 
-var scaling = 0.5;
+var thermoclines = [{depth: 120, attenuation: 0.01}, {depth: 240, attenuation: 0.01}, {depth: 600, attenuation: 0.001}, {depth: 1200, attenuation: 0.001}];
+
+var friends = {
+  'USA': ['USA', 'Great Britain', 'Australia'],
+  'Great Britain': ['Great Britain', 'Australia', 'USA'],
+  'Australia': ['Australia', 'Great Britain', 'USA'],
+  'Russia': ['Russia', 'India', 'China'],
+  'China': ['China', 'Russia', 'India'],
+  'India': ['India', 'Russia', 'China']
+};
+
+var enemies = {
+  'USA': ['Russia', 'China', 'India'],
+  'Great Britain': ['Russia', 'China', 'India'],
+  'Australia': ['Russia', 'China', 'India'],
+  'Russia': ['USA', 'Great Britain', 'Australia'],
+  'China': ['USA', 'Great Britain', 'Australia'],
+  'India': ['USA', 'Great Britain', 'Australia']
+};
+
+var scaling = 1;
 
 var drawGrid = function (ctx, width, height, triton) {
-  width *= 0.5 / scaling; height *= 0.5 / scaling;
-  var shift = triton === undefined ? {x: 0, y: 0} : {x: - triton . position . x, y: - triton . position . y};
-  ctx . scale (scaling, scaling);
+  width *= 0.5; height *= 0.5;
+  var mile = 128 * scaling;
+  var shift = triton === undefined ? {x: 0, y: 0} : {x: - triton . position . x * mile, y: - triton . position . y * mile};
   ctx . translate (width + shift . x, height + shift . y);
   ctx . beginPath ();
   ctx . strokeStyle = 'yellow';
@@ -22,11 +43,11 @@ var drawGrid = function (ctx, width, height, triton) {
   var limit_right = - shift . x + width;
   var limit_top = - shift . y - height;
   var limit_bottom = - shift . y + height;
-  var grid_left = Math . floor (limit_left / 128) * 128 + 128;
-  var grid_right = Math . floor (limit_right / 128) * 128;
-  var grid_top = Math . floor (limit_top / 128) * 128 + 128;
-  var grid_bottom = Math . floor (limit_bottom / 128) * 128;
-  for (var ind = grid_left; ind <= grid_right; ind += 128) {ctx . moveTo (ind, limit_top); ctx . lineTo (ind, limit_bottom);}
-  for (var ind = grid_top; ind <= grid_bottom; ind += 128) {ctx . moveTo (limit_left, ind); ctx . lineTo (limit_right, ind);}
+  var grid_left = Math . floor (limit_left / mile) * mile + mile;
+  var grid_right = Math . floor (limit_right / mile) * mile;
+  var grid_top = Math . floor (limit_top / mile) * mile + mile;
+  var grid_bottom = Math . floor (limit_bottom / mile) * mile;
+  for (var ind = grid_left; ind <= grid_right; ind += mile) {ctx . moveTo (ind, limit_top); ctx . lineTo (ind, limit_bottom);}
+  for (var ind = grid_top; ind <= grid_bottom; ind += mile) {ctx . moveTo (limit_left, ind); ctx . lineTo (limit_right, ind);}
   ctx . stroke ();
 };
--- triton.js
+++ triton.js
@@ -1,64 +1,162 @@
 
-var triton = function () {
+var triton = function (country) {
 	this . position = {x: 0, y: 0, depth: 0, bearing: 0};
-	this . speed = {x: 1, y: 0};
+	this . speed = {x: 0, y: 0};
+	this . diving_speed = 0;
+	this . bearing_speed = 0;
+	this . depth_target = 0;
+	this . bearing_target = 0;
+	this . noise = 0;
 	this . trail_delta = 0;
 	this . selected = false;
 	this . delete = false;
 	this . trail = [];
+	this . country = country;
+	this . type = 'submarine';
+	this . status = 'unknown';
+	this . speeds = [0, 2, 8, 16, 24, 32, 40];
+	this . noises = [0, 120, 480, 4800, 30000, 190000, 1200000];
+	this . bearing_speeds = [0, 10, 20, 30, 40, 50, 60];
+	this . diving_speeds = [0, 10, 20, 30, 40, 50, 60];
 };
+
+triton . prototype . noiseLevel = function () {return this . noise;};
 
 triton . prototype . move = function (delta) {
 	this . trail_delta --;
-	if (this . trail_delta < 1) {this . trail . push ({x: this . position . x, y: this . position . y}); this . trail_delta = 5;}
-	this . position . x += delta * (Math . cos (this . position . bearing) * this . speed . x - Math . sin (this . position . bearing) * this . speed . y);
-	this . position . y += delta * (Math . cos (this . position . bearing) * this . speed . y + Math . sin (this . position . bearing) * this . speed . x);
+	if (this . trail_delta < 1) {this . trail . push ({x: this . position . x * 128, y: this . position . y * 128}); this . trail_delta = 0.002 / delta;}
+	var bearing = (this . position . bearing - 90) * Math . PI / 180;
+	var sdelta = delta / 3600;
+	this . position . x += sdelta * (Math . cos (bearing) * this . speed . x - Math . sin (bearing) * this . speed . y);
+	this . position . y += sdelta * (Math . cos (bearing) * this . speed . y + Math . sin (bearing) * this . speed . x);
+	if (this . position . depth != this . depth_target) {
+		var dspeed = this . diving_speed * delta;
+		if (Math . abs (this . depth_target - this . position . depth) <= dspeed) {this . position . depth = this . depth_target; this . diving_speed = 0;}
+		else {
+			if (this . depth_target > this . position . depth) this . position . depth += dspeed;
+			else this . position . depth -= dspeed;
+		}
+		console . log (dspeed, this . diving_speed, this . position);
+	}
 };
 
 triton . prototype . simulate = function (delta) {this . move (delta);};
+
+triton . prototype . setSpeed = function (index) {
+	switch (index) {
+		case 'stop': index = 0; break;
+		case 'slow': index = 1; break;
+		case 'one quarter': index = 2; break;
+		case 'half': index = 3; break;
+		case 'three quarters': index = 4; break;
+		case 'full': index = 5; break;
+		case 'flank': index = 6; break;
+		default: index = 0; break;
+	}
+	if (index < 0) {this . noise = this . noises [0]; this . speed = {x: 0, y: 0}; return;}
+	if (index >= this . speeds . length) {this . noise = this . noises [this . speeds . length - 1]; this . speed = {x: this . speeds [this . speeds . length - 1], y: 0}; return;}
+	this . noise = this . noises [index]; this . speed = {x: this . speeds [index], y: 0};
+};
+
+triton . prototype . targetDepth = function (depth) {
+	this . diving_speed = this . diving_speeds [this . diving_speeds . length - 1];
+	if (typeof (depth) === 'number') {this . depth_target = depth; return;}
+	switch (depth) {
+		case 'surface': this . depth_target = 0; break;
+		case 'periscope': this . depth_target = 60; break;
+		case 'down thermal':
+			for (var ind in thermoclines) {
+				if (thermoclines [ind] . depth > this . position . depth) this . depth_target = thermoclines . depth + 18;
+				return;
+			}
+			break;
+		case 'up thermal':
+			for (var ind = thermoclines . length - 1; ind >= 0; ind++) {
+				if (thermoclines [ind] . depth < this . position . depth) this . depth_target = thermoclines . depth - 18;
+			}
+			break;
+		default: break;
+	}
+};
 
 triton . prototype . draw = function (ctx) {
 	ctx . strokeStyle = 'white';
 	ctx . lineWidth = 1;
 	for (var ind in this . trail) {
 		ctx . beginPath ();
-		ctx . arc (this . trail [ind] . x, this . trail [ind] . y, 1, 0, 6.28);
+		ctx . arc (this . trail [ind] . x * scaling, this . trail [ind] . y * scaling, 1, 0, 6.28);
 		ctx . stroke ();
 	}
-	ctx . strokeStyle = 'red';
-	ctx . fillStyle = 'red';
+	var x = this . position . x * scaling * 128, y = this . position . y * scaling * 128;
+	switch (this . status) {
+		case 'friend': ctx . fillStyle = ctx . strokeStyle = 'lime'; break;
+		case 'enemy': ctx . fillStyle = ctx . strokeStyle = 'red'; break;
+		case 'neutral': ctx . fillStyle = ctx . strokeStyle = 'white'; break;
+		case 'simulation': ctx . fillStyle = ctx . strokeStyle = 'gray'; break;
+		default: ctx . strokeStyle = 'white'; break;
+	}
 	ctx . lineCap = 'square';
 	ctx . lineWidth = this . selected ? 3 : 2;
-	ctx . beginPath (); ctx . arc (this . position . x, this . position . y, 2, 0, 6.28); ctx . fill ();
 	ctx . beginPath ();
-	ctx . moveTo (this . position . x + 8, this . position . y - 8);
-	ctx . lineTo (this . position . x + 8, this . position . y + 8);
-	ctx . lineTo (this . position . x - 8, this . position . y + 8);
-	ctx . lineTo (this . position . x - 8, this . position . y - 8);
-	ctx . lineTo (this . position . x + 8, this . position . y - 8);
+	switch (this . type) {
+		case 'surface':
+			ctx . beginPath (); ctx . arc (x, y, 2, 0, 6.28); ctx . fill ();
+			ctx . moveTo (x + 8, y - 8); ctx . lineTo (x + 8, y + 8); ctx . lineTo (x - 8, y + 8);
+			ctx . lineTo (x - 8, y - 8);ctx . lineTo (x + 8, y - 8); break;
+		case 'submarine':
+			if (this . status === 'simulation') {
+				var bearing = (this . position . bearing - 90) * Math . PI / 180;
+				var alpha = Math . cos (bearing) * 12, beta = Math . sin (bearing) * 12;
+				ctx . lineCap = 'round'
+				ctx . lineWidth = 4; ctx . moveTo (x - alpha, y - beta); ctx . lineTo (x + alpha, y + beta);
+			}
+			else {
+				ctx . beginPath (); ctx . arc (x, y, 2, 0, 6.28); ctx . fill ();
+				ctx . moveTo (x + 8, y); ctx . lineTo (x, y + 8); ctx . lineTo (x - 8, y);
+			}
+			break;
+		default: break;
+	}
 	ctx . stroke ();
 };
 
-var Virginia = function (name) {
-	triton . call (this);
+triton . prototype . checkStatusOf = function (vessel) {
+	if (this === vessel) return 'simulation';
+	if (friends [this . country] . includes (vessel . country)) return 'friend';
+	if (enemies [this . country] . includes (vessel . country)) return 'enemy';
+	return 'neutral';
+};
+
+triton . prototype . getVectorFrom = function (vessel) {return {x: vessel . position . x - this . position . x, y: vessel . position . y - this . position . y};};
+
+triton . prototype . getRelativePositionOf = function (vessel) {
+	var vector = this . getVectorFrom (vessel);
+	vector . distance = Math . sqrt (vector . x * vector . x + vector . y * vector . y);
+	vector . bearing = Math . atan2 (vector . y, vector . x);
+	return vector;
+};
+
+triton . prototype . getNoiseOf = function (vessel) {
+	var vector = this . getRelativePositionOf (vessel);
+	noise = vessel . noiseLevel ();
+	if (vector . distance > 0) noise /= vector . distance * 1852;
+	return noise;
+};
+
+var Virginia = function (name, country) {
+	if (country === undefined) country = 'USA';
+	triton . call (this, country);
 	this . class = 'Virginia';
 	this . name = name;
+	this . speeds = [0, 2, 8, 15, 19, 25, 35];
 };
 Virginia . prototype = Object . create (triton . prototype);
-/*
-var create_triton = function (triton_class, name) {
-	var triton = function () {triton_class . call (this); this . name = name;};
-	triton . prototype = Object . create (triton_class . prototype);
-	return triton;
+
+var Akula = function (name, country) {
+	if (country === undefined) country = 'Russia';
+	triton . call (this, country);
+	this . class = 'Akula';
+	this . name = name;
+	this . speeds = [0, 2, 8, 15, 21, 28, 35];
 };
-
-var VirginiaClass = function () {
-	triton . call (this);
-	this . class = 'Virginia';
-}
-VirginiaClass . prototype = Object . create (triton . prototype);
-
-var Virginia = create_triton (VirginiaClass, 'Virginia');
-var Hawaii = create_triton (VirginiaClass, 'Hawaii');
-var Texas = create_triton (VirginiaClass, 'Texas');
-*/
+Akula . prototype = Object . create (triton . prototype);
