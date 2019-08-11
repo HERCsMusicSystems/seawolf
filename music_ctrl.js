ab94ab99ac64828aa12f3f01f0fd5577ab4917a7
rama <rama@rama.org.au>
2019-08-11 21:19:57 +0930

    Speech synthesis added.

--- music_ctrl.js
+++ music_ctrl.js
@@ -1,5 +1,16 @@
 
 
+var dialect = document . getElementById ('dialect');
+var dialects = speechSynthesis . getVoices ();
+if (dialect !== null) {
+	for (var ind in dialects) {
+		var el = document . createElement ('option');
+		el . textContent = dialects [ind] . name;
+		el . value = ind;
+		dialect . appendChild (el);
+	}
+}
+
 var create_audio = function (file) {
 	var div = document . createElement ('div');
 	div . innerHTML = `
@@ -21,3 +32,7 @@ var music = {
 var PlayMusic = function (id) {music [id] . play ();};
 var PauseMusic = function (id) {music [id] . pause ();};
 var LoopMusic = function (id, loop) {music [id] . loop = loop;};
+
+
+var say = function (word) {word = new SpeechSynthesisUtterance (word); word . voice = dialects [dialect . value]; speechSynthesis . speak (word);};
+
