e654a19f9eb889b28b62df68c6ab122842b8ec16
robert <robertwolf@consunet.com.au>
2019-08-26 16:21:36 +0930

    WIP.

--- utilities.js
+++ utilities.js
@@ -5,12 +5,12 @@ var ranks = {
   Russia: ['Капитан-лейтенант', 'Капитан третьего ранга', 'Капитан второго ранга', 'Капитан']
 };
 
+var side_name = localStorage . getItem ('side');
+var side = JSON . parse (localStorage . getItem (side_name));
+var captain = side . captains [side . captain];
+
 var promote = function () {
-  var side_name = localStorage . getItem ('side');
-  var side = JSON . parse (localStorage . getItem (side_name));
-  var captain = side . captains [side . captain];
   var ret = 'No promotion this time.';
-  console . log (captain . rank_id, ranks [side_name] . length);
   if (captain . rank_id < ranks [side_name] . length - 1) {
     captain . rank_id += 1;
     ret = `${captain . rank} ${side . captain} will be promoted to ${ranks [side_name] [captain . rank_id]}`;
@@ -18,4 +18,9 @@ var promote = function () {
     localStorage . setItem (side_name, JSON . stringify (side));
   }
   return ret;
+};
+
+var SimulatedMission = function () {
+  if (localStorage . getItem ('mission_type') === 'Simulated' || captain . rank_id < 2) return true;
+  return false;
 };
