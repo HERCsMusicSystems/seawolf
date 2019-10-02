01ca422e29ac1812ee61d6aae100a309ef989138
robert <robertwolf@consunet.com.au>
2019-10-02 13:23:55 +0930

    Akula work....

--- artificial_intelligence.js
+++ artificial_intelligence.js
@@ -120,6 +120,8 @@ var akulaAI = function (akula) {
 		case 'searching':
 			if (akula . position . depth === akula . test_depth) akula . targetDepth ('periscope', 1);
 			if (akula . position . depth === 60) akula . targetDepth ('test', 1);
+			var target = akula . sonar . detectStrongest (delta, 'submarine');
+			if (target !== null) {console . log (target); this . mode = 'waypoint';}
 			break;
 		case 'waypoint': break;
 		}
