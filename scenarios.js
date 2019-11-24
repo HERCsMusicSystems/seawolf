a748ba2e2799277d05c476b2241fbaa87d524d69
rama <robert.wolf@hercsmusicsystems.com>
2019-11-24 20:33:20 +1030

    Invasion....

--- scenarios.js
+++ scenarios.js
@@ -4,7 +4,8 @@ var scenarios = {
 		{Title: 'Easy Detection', Instructions: 'Confirm there is an Oil Tanker'},
 		{Title: 'Three Oilers', Instructions: 'Sink three Oil Tankers'},
 		{Title: 'Easy Convoy', Instructions: 'Sink three Oil Tankers, but watch out for Sovremenny\s escort.'},
-		{Title: 'Akula is out there', Instructions: 'Akula is chasing you. Sink it.'}
+		{Title: 'Akula is out there', Instructions: 'Akula is chasing you. Sink it.'},
+		{Title: 'Invasion', Instructions: 'Stop the invasion.'}
 	],
 	Russia: [
 		{Title: 'Three Oilers', Instructions: 'Sink three Oil Tankers'}
