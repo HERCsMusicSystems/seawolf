f551a28f450967b69dd7b229ef5943f6148d4e94
rama <robert.wolf@hercsmusicsystems.com>
2019-12-03 23:10:30 +1030

    Kirov scenario started.

--- scenarios.js
+++ scenarios.js
@@ -5,7 +5,8 @@ var scenarios = {
 		{Title: 'Three Oilers', Instructions: 'Sink three Oil Tankers'},
 		{Title: 'Easy Convoy', Instructions: 'Sink three Oil Tankers, but watch out for Sovremenny\s escort.'},
 		{Title: 'Akula is out there', Instructions: 'Akula is chasing you. Sink it.'},
-		{Title: 'Invasion', Instructions: 'Stop the invasion.'}
+		{Title: 'Invasion', Instructions: 'Stop the invasion.'},
+		{Title: 'Kirov is out there', Instructions: 'Find and eliminate Kirov.'}
 	],
 	Russia: [
 		{Title: 'Three Oilers', Instructions: 'Sink three Oil Tankers'}
