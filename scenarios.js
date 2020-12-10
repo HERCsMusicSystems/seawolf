9ad577a28023fee84aa9dc0811f250f25153621a
robert <robertwolf@consunet.com.au>
2020-12-10 11:47:02 +1030

    Scenario renamed to Typhoon's den.

--- scenarios.js
+++ scenarios.js
@@ -5,7 +5,7 @@ var scenarios = {
 		{Title: 'Three Oilers', Instructions: 'Sink three Oil Tankers', rank: 1},
 		{Title: 'Easy Convoy', Instructions: 'Sink three Oil Tankers, but watch out for Sovremenny\s escort.', rank: 2},
 		{Title: 'Akula is out there', Instructions: 'Akula is chasing you. Sink it.', rank: 2},
-		{Title: 'Training facility', Instructions: 'There is a guerilla trainig facility. Destroy it.', rank: 2},
+		{Title: 'Typhoon\'s den', Instructions: 'Typhoon is hinding in its safe port. Sink it.', rank: 2},
 		{Title: 'Invasion', Instructions: 'Stop the invasion.', rank: 3},
 		{Title: 'Kirov is out there', Instructions: 'Find and eliminate Kirov.', rank: 3},
 		{Title: 'Terror of Peter the Great', Instructions: 'Find and eliminate Peter the Great.', rank: 4}
