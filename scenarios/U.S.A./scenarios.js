1c19385886a61f309a836b93b82ff4ee6bc64ead
robert <robertwolf@consunet.com.au>
2020-12-14 07:48:50 +1030

    Superfluous folder removed.

--- scenarios/U.S.A./scenarios.js
+++
@@ -1,6 +0,0 @@
-
-scenarios ['U.S.A.'] = [
-  {Title: 'Three Oilers', Instructions: 'Sink three Oil Tankers'},
-  {Title: 'Easy Convoy', Instructions: 'Sink three Oil Tankers, but watch out for Sovremenny\s escort.'},
-  {Title: 'Akula is out there', Instructions: 'Akula is chasing you. Sink it.'}
-];
